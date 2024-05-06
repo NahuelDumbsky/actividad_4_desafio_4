b94919950ce4f92f60f23153c49fd5396ad985cc
Nahuel_Dumbsky <dumbskynahuel@gmail.com>
2024-05-06 16:18:37 -0300

    se agrega actividad 4

--- actividad_4_desafio_4/actividad4_desafio4.java
+++ actividad_4_desafio_4/actividad4_desafio4.java
@@ -10,7 +10,7 @@ public class actividad4_desafio4 {
         // instancio la letra con la que quiero filtrar las palabras
         char letter = 'c';
 
-        // Filtro palabras que empiecen o contengan la letra 'c'
+        // Filtro palabras que empiecen o contengan la letra 'C'
         words.stream()
                 .filter(word -> word.contains(String.valueOf(letter)))
                 // Muestro en pantalla la lista ya filtrada
